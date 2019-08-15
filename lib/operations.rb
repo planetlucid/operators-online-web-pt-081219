646cb0da6ce443927b7a9f5f03505715457747c6
Ryan S <planetlucid@gmail.com>
2019-08-15 03:56:06 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,23 @@
 def unsafe?(speed)
-
+  if speed == 45
 end
-
-
+  elsif speed > 60
+end
+  else speed < 40
+end    
+  return false
+end 
+end
 
 def not_safe?(speed)
-	
+  speed > 60 ? "true" : "safe"
 end
+	speed < 40 ? "true" : "safe"
+end
+	speed == 35 ? "true" : "safe"
+end
+end
+
 	
 
 
