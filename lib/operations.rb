80737f115bdcc00c9ff8336a006728a10f1e7802
Ryan S <planetlucid@gmail.com>
2019-08-15 03:55:24 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,10 @@
 def unsafe?(speed)
   if speed == 45
+end
   elsif speed > 60
+end
   else speed < 40
+end    
   return false
 end 
 
@@ -13,7 +16,7 @@ end
 end
 	speed == 35 ? "true" : "safe"
 end
-end
+
 	
 
 
